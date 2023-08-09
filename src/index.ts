328178b6eb5dd065eb79fb942dca7f39c119501e
natty-light <75773163+natty-light@users.noreply.github.com>
2023-08-09 16:11:16 -0400

    Add JSDoc

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,21 @@ type BatchedJobProgressInformation = {
   total: number,
 }
 
+/**
+ * Splits records into batches of length specified in batchingOptions.transactionSize, then calls executor on
+ * each chunk, calling optional onSuccess or onFail callbacks as approriate, then applies a reducer method
+ * provided at reducerOptions.reducer.
+ * 
+ * Executor must take in a single argument, the results. If the asynchronous function requires multiple static inputs,
+ * you must pass in a closure: const executor = (results) => consumer(results, ...staticOptions);
+ * @param executor Function or closure to be called on each chunk of records.
+ * @param records Input for executor.
+ * @param reducerOptions Struct containing reducer and accumulator. Implementation mimics Array.prototype.reduce().
+ * @param batchingOptions Struct containing configuration options for batching.
+ * @returns Concatenated results of all batched jobs
+ */
+
+
 async function batchJob<T, K, U>(
   executor: BatchedJobExecutor<T, K>,
   records: T[],
