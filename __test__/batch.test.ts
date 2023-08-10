a305be645fdfe16362560f1a60c25ef88ca596b0
natty-light <75773163+natty-light@users.noreply.github.com>
2023-08-10 10:18:49 -0400

    Add new `concurrentBatchJob` - Uses `Promise.all` - Add new test

--- __test__/batch.test.ts
+++ __test__/batch.test.ts
@@ -1,4 +1,4 @@
-import { batchJob } from '../src/index';
+import { batchJob, concurrentBatchJob } from '../src/index';
 import type { BatchedJobExecutor, BatchedJobOptions, BatchedJobReducerOptions } from '../src/types'
 
 describe('batch', () => {
@@ -37,3 +37,40 @@ describe('batch', () => {
   })
 })
 
+
+
+describe('concurrent batch', () => {
+  const executor: BatchedJobExecutor<number, number> = async (records: number[]) => {
+    return new Promise((resolve) => {
+      let initialVal = 0;
+      setTimeout(() => resolve({
+        ok: true,
+        result: records.reduce((val, acc) => val + acc, initialVal)
+      }),10)
+    })
+  };
+
+  const records: number[] = Array.from({length: 2_000_000}, (_, i) => i);
+
+  const reducerOptions: BatchedJobReducerOptions<number, number> = {
+    reducer: (val, acc) => val + acc,
+    accumulator: 0,
+  };
+
+  const onSuccess = jest.fn((res) => res);
+  const onError = jest.fn((err) => err);
+  
+  const jobOptions: BatchedJobOptions<number> = {
+    transactionSize: 50_000,
+    retry: false,
+    onBatchSuccess: onSuccess,
+    onBatchError: onError
+  }
+
+  it('Successful writes', async () => {
+      const result = await concurrentBatchJob(executor, records, reducerOptions, jobOptions)
+      expect(result).toBe(1999999000000);
+      expect(onSuccess).toBeCalledTimes(40);
+      expect(onError).not.toBeCalled();
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -49,3 +49,49 @@ export async function batchJob<T, K, U>(
   }
   return endResult;
 };
+
+/**
+ * Splits records into batches of length specified in batchingOptions.transactionSize, then calls executor on
+ * each chunk concurrently using Promise.all(), calling optional onSuccess or onFail callbacks as approriate, then applies a reducer method
+ * provided at reducerOptions.reducer. Retries not currently supported in this function.
+ * 
+ * Executor must take in a single argument, the results. If the asynchronous function requires multiple static inputs,
+ * you must pass in a closure: const executor = (results) => consumer(results, ...staticOptions);
+ * @param executor Function or closure to be called on each chunk of records.
+ * @param records Input for executor.
+ * @param reducerOptions Struct containing reducer and accumulator. Implementation mimics Array.prototype.reduce().
+ * @param batchingOptions Struct containing configuration options for batching.
+ * @returns Concatenated results of all batched jobs
+ */
+export async function concurrentBatchJob<T, K, U>(
+  executor: BatchedJobExecutor<T, K>,
+  records: T[],
+  reducerOptions: BatchedJobReducerOptions<K, U>,
+  batchingOptions: BatchedJobOptions<K>
+  ): Promise<U> {
+  const { reducer, accumulator } = reducerOptions;
+  const { transactionSize, onBatchSuccess, onBatchError } = batchingOptions;
+  const total = records.length;
+
+  const chunks: T[][] = [];
+  for (let i = 0; i < total; i += transactionSize) {
+    chunks.push(records.slice(i, i + transactionSize));
+  }
+
+  const batchResults = await Promise.all(chunks.map((chunk) => executor(chunk)));
+
+  let endResult: U = accumulator;
+  batchResults.forEach((batchResult) =>{
+    if (!batchResult.ok) {
+      if (onBatchError) onBatchError(batchResult.error);
+      return;
+    }
+
+    const { result } = batchResult;
+
+    if (onBatchSuccess) onBatchSuccess(result);
+    endResult = reducer(result, endResult);
+  });
+
+  return endResult;
+};
